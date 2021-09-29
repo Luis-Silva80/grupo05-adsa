8addc0448c34b0232dd6d42c03e0565e4ee888f6
Luis-Silva80 <luis.silva@bandtec.com.br>
2021-09-29 19:16:52 -0300

    subindo para testar com o front

--- Spring/api-tothlib/src/main/java/b/com/tothlibs/apitothlib/controlers/AdminController.java
+++ Spring/api-tothlib/src/main/java/b/com/tothlibs/apitothlib/controlers/AdminController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 @Controller
