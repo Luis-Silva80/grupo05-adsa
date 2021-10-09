08c283f2adf9ef769f959e747339998b553cbe81
Luis-Silva80 <luis.silva@bandtec.com.br>
2021-10-09 18:04:20 -0300

    refactor: ajustar configs e formatação do swagger

--- Spring/api-tothlib/src/main/java/b/com/tothlibs/apitothlib/controlers/BibliotecaController.java
+++ Spring/api-tothlib/src/main/java/b/com/tothlibs/apitothlib/controlers/BibliotecaController.java
@@ -4,7 +4,10 @@ import b.com.tothlibs.apitothlib.entity.Livros;
 import b.com.tothlibs.apitothlib.entity.PerfilUsuario;
 import b.com.tothlibs.apitothlib.repository.LivrosRepository;
 import b.com.tothlibs.apitothlib.repository.PerfilUsuarioRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,8 @@ import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/livros")
+@Api(value = "API REST")
+@CrossOrigin(origins = "*")
 public class BibliotecaController {
 
     public static final Logger LOGGER = Logger.getLogger(String.valueOf(BibliotecaController.class));
@@ -24,30 +29,67 @@ public class BibliotecaController {
     private PerfilUsuarioRepository repositoryUsuario;
 
     @GetMapping
+    @ApiOperation(value = "Retorna uma lista de livros")
     public @ResponseBody
     Iterable<Livros> getLivros() {
         return repository.findAll();
     }
 
-    @PostMapping("/{idUsuario}")
+    @PostMapping("/{idLivro}")
     public @ResponseBody
-    Boolean postLivros(@PathVariable Integer idUsuario, @RequestBody Livros livros) {
+    @ApiOperation(value = "Realiza um cadastro de um livro")
+    Boolean postLivros(@PathVariable Integer idUsuario, @RequestBody Livros idLivro) {
 
         Integer admin = repositoryUsuario.findAdminById(idUsuario);
 
         if (admin.equals(1)) {
 
-            livros.setQtdResenhas(0);
-            livros.setQtdReservas(0);
-            livros.setQtdReservadoAgora(0);
-            livros.setStatusLivro("disponivel");
+            idLivro.setQtdResenhas(0);
+            idLivro.setQtdReservas(0);
+            idLivro.setQtdReservadoAgora(0);
+            idLivro.setStatusLivro("disponivel");
 
-            repository.save(livros);
+            repository.save(idLivro);
             return true;
 
         }
 
         return false;
+
+    }
+
+    @GetMapping("/{idLivro}")
+    @ApiOperation(value = "Retorna um usuario por um ID especifico")
+    public ResponseEntity exibeUsuarioAdmin(@PathVariable Integer idLivro){
+
+        LOGGER.info("Retornando usuario desejado...");
+        return ResponseEntity.of(repository.findById(idLivro));
+
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Realiza uma alteração no cadastro de livros")
+    public ResponseEntity alteraLivro(@PathVariable Integer id, @RequestBody Livros livroAtualizado){
+        if (repository.existsById(id)) {
+            livroAtualizado.setId(id);
+            repository.save(livroAtualizado);
+            return ResponseEntity.status(200).build();
+        } else {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Remove um livro pelo indice")
+    public ResponseEntity deletaPorId(@PathVariable Integer id){
+
+        repository.deleteById(id);
+
+        if(repository.existsById(id)){
+            return ResponseEntity.status(400).build();
+        }else {
+            return ResponseEntity.status(200).build();
+        }
 
     }
 }
