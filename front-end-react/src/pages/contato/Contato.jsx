5ac165f712a3ef8f15ed4d487d8da817ddd12f29
Luis-Silva80 <luis.silva@bandtec.com.br>
2021-10-10 16:02:22 -0300

    feature: adicionar page contato para react

---
+++ front-end-react/src/pages/contato/Contato.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import "./style.scss";
+
+// import components-
+import Footer from '../../components/footer/Footer';
+import SideBar from '../../components/sideBar/SideBar';;
+
+// import img from '../../assets/';
+
+function Contato() {
+    return (
+        <>
+            <SideBar />
+            <main className="main container">
+                <h1 className="main_title">Contato</h1>
+                <div className="main_text">
+                    <p>
+                        Sugestões de livros, melhorias, críticas...
+                    </p>
+                    <p>
+                        Preencha o formulário abaixo para entrar em contato com
+                    </p>
+                    <p>
+                        a nossa equipe de atendimento!
+                    </p>
+                </div>
+                <form action="" className="main_form">
+                    <input className="main_input" type="text" placeholder="Informe o assunto" />
+                    <textarea className="main_textarea" placeholder="Descrição" name="" id="" cols="30" rows="6"></textarea>
+                </form>
+                <div className="main_box">
+                    <label className="main_box_label">
+                        O prazo de resposta é de até 48 horas!
+                    </label>
+                    <button className="main_box_button">
+                        Enviar
+                    </button>
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default Contato;
