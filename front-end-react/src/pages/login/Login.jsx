efeca4c5296b56237e29a0196481c18b6fef42f2
LucasDonizete08 <lucas.dsantos@bandtec.com.br>
2021-10-10 16:10:48 -0300

    feature: Adicionado as páginas login e livro para react.

---
+++ front-end-react/src/pages/login/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import "./style.scss";
+
+import Footer from '../../components/footer/Footer';
+import SideBar from '../../components/sideBar/SideBar';
+
+import api from "../../services/api";
+import { Link } from 'react-router-dom';
+
+
+function Login()
+{
+    function Submit(event) {
+    //
+    //    event.preventDefault();
+    //
+    //  const formData = {
+    //        email: document.getElementById("email").value,
+    //        password: document.getElementById("password").value
+    //    };
+    //
+    //    console.log(formData);
+    //
+    //    api({
+    //        method: 'get',
+    //        url: '/aluno/1',
+    //        data: formData,
+    //    }).then(function (response) {
+    //        console.log(response.data)
+    //    });
+    };
+
+    
+return (
+<>
+    <SideBar />
+
+    <main className="main container">
+      <h1 className="main_title">Login</h1>
+        <p className="main_parag">Preencha os campos abaixo para acessar a sua conta.</p>
+          <form action="" className="main_form" id="form" onsubmit={Submit}>
+              <input type="text" id="email" name="email" required className="main_form_input" placeholder="Email: usuario.exemplo@email.com"/>
+              <input type="password" id="password" name="password" required className="main_form_input" placeholder="Senha: *************"/>
+              <input type="submit" className="main_form_button" id="formBtn" value="Entrar" />
+          </form>
+            <a className="main_forgot" href="#">Esqueci a minha senha</a>
+            <div className="main_box">
+                <p className="main_box_parag">Ainda não tem conta?</p>
+            <a classNameName="main_box_link" href="cadastro.html">Cadastrar</a>
+            </div>
+            <section id="resp" className="resp"></section>
+        </main>
+
+        <Footer />
+    </>
+);
+}
+export default Login;
--- front-end-react/src/routes.js
+++ front-end-react/src/routes.js
@@ -5,6 +5,10 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CadastroUsuario from "./pages/cadastroUsuario/CadastroUsuario";
 import Home from "./pages/home/Home";
 import Faq from "./pages/faq/Faq";
+import Login from "./pages/login/Login";
+import Livro from "./pages/livro/Livro";
+
+
 
 
 export default function Routes() {
@@ -14,6 +18,9 @@ export default function Routes() {
                 <Route path="/" exact component={Home} />
                 <Route path="/cadastroUsuario" exact component={CadastroUsuario} />
                 <Route path="/faq" exact component={Faq} />
+                <Route path="/login" exact component={Login} />
+                <Route path="/livro" exact component={Livro} />
+
             </Switch>
         </BrowserRouter>
     );
