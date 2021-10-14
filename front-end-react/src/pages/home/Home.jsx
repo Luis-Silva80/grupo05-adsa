1ddab26391ab1fc9d7a01b83d893bb1610bbb342
lucas alves <lucas.pereira@codeby.com.br>
2021-10-13 22:16:50 -0300

    finishing main front end

--- front-end-react/src/pages/home/Home.jsx
+++ front-end-react/src/pages/home/Home.jsx
@@ -12,8 +12,7 @@ import './style.scss';
 // import imgs
 import mainImg from '../../assets/main-image.png';
 import quemSomos from '../../assets/quemSomos.png';
-import graph1 from '../../assets/graph.png';
-import graph2 from '../../assets/graph (1).png';
+import oqFazemos from '../../assets/oqFazemos.png';
 import arrowScroll from '../../assets/arrowScroll.png';
 
 
@@ -23,28 +22,34 @@ function Home() {
             <Header />
 
                 <section id="rootHome">
-                    <main className="homeMain">
-                        <div className="homeMain-box">
-                            <h1 className="homeMain-box-title">ThothLib</h1>
-                            <p className="homeMain-box-parag">Especialistas em Softwares de gestão e ERP.</p>
-                            <Link className="homeMain-box-link">Saiba mais</Link>
+                    <main className="main">
+                        <div className="main-box">
+                            <h1 className="main-box-title">ThothLib</h1>
+                            <p className="main-box-parag">Especialistas em Softwares de gestão e ERP.</p>
+                            <Link className="main-box-link">Saiba mais</Link>
                         </div>
-                        <img className="homeMain-img" src={mainImg} alt="TothLib main banner" />
-                        <img className="homeMain-arrowScroll" src={arrowScroll} alt="TothLib arrow scroll" />
+                        <img className="main-img" src={mainImg} alt="TothLib main banner" />
+                        <img className="main-arrowScroll" src={arrowScroll} alt="TothLib arrow scroll" />
                         
                     </main>
 
-                    <section className="homeSection">
-                        <div className="homeSection-box">
-                            <h3 className="homeSection-box-title">Quem Somos</h3>
+                    <section className="about">
+                        <div className="about-box">
+                            <h3 className="about-box-title">Quem Somos</h3>
                             
-                            <p className="homeSection-box-parag">
+                            <p className="about-box-parag">
                             Antes de tudo SOMOS UM TIME!
                             </p>
-                            <p className="homeSection-box-parag">Apaixonados por tecnologia e desenvolvimento, formamos uma fábrica de softwares especializados em gestão e controle de produtos. </p>
+                            <p className="about-box-parag">Apaixonados por tecnologia e desenvolvimento, formamos uma fábrica de softwares especializados em gestão e controle de produtos. </p>
                         </div>
-                        <img className="homeSection-img" src={quemSomos} alt="TothLib quem somos" />
-                        <img className="homeSection-graph" src={graph2} alt="TothLib graph" />
+                        <img className="about-img" src={quemSomos} alt="TothLib quem somos" />
+                    </section>
+                    <section className="weDo reverse">
+                        <div className="weDo-box">
+                            <h3 className="weDo-box-title">O que fazemos?</h3>
+                            <p className="weDo-box-parag">Somos uma fábrica de softwares, especializados em gestão e controle de produtos. Nossos sístemas garantem mais segurança, controle, agilidade, e é escalável para diversos modelos de negócios.</p>
+                        </div>
+                        <img className="weDo-img" src={oqFazemos} alt="TothLib quem somos" />
                     </section>
                 </section>
                 
