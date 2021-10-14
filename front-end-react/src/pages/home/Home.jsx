840f25bf0f9828f7d195528c5f997d243c062ce4
lucas alves <lucas.pereira@codeby.com.br>
2021-10-13 22:49:23 -0300

    adding AOS

--- front-end-react/src/pages/home/Home.jsx
+++ front-end-react/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 // React and hooks
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
 
 // import components
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
 // import css
+// import "https://unpkg.com/aos@2.3.1/dist/aos.css";
 import './style.scss';
 
 // import imgs
@@ -17,10 +19,10 @@ import arrowScroll from '../../assets/arrowScroll.png';
 
 
 function Home() {
+    // AOS.init();
     return(
         <>
             <Header />
-
                 <section id="rootHome">
                     <main className="main">
                         <div className="main-box">
@@ -30,20 +32,17 @@ function Home() {
                         </div>
                         <img className="main-img" src={mainImg} alt="TothLib main banner" />
                         <img className="main-arrowScroll" src={arrowScroll} alt="TothLib arrow scroll" />
-                        
                     </main>
-
+                    {/* <section className="about" data-aos="fade-right"> */}
                     <section className="about">
                         <div className="about-box">
                             <h3 className="about-box-title">Quem Somos</h3>
-                            
-                            <p className="about-box-parag">
-                            Antes de tudo SOMOS UM TIME!
-                            </p>
+                            <p className="about-box-parag">Antes de tudo SOMOS UM TIME!</p>
                             <p className="about-box-parag">Apaixonados por tecnologia e desenvolvimento, formamos uma fábrica de softwares especializados em gestão e controle de produtos. </p>
                         </div>
                         <img className="about-img" src={quemSomos} alt="TothLib quem somos" />
                     </section>
+                    {/* <section className="weDo reverse" data-aos="fade-left"> */}
                     <section className="weDo reverse">
                         <div className="weDo-box">
                             <h3 className="weDo-box-title">O que fazemos?</h3>
@@ -52,8 +51,6 @@ function Home() {
                         <img className="weDo-img" src={oqFazemos} alt="TothLib quem somos" />
                     </section>
                 </section>
-                
-            
             <Footer />
         </>
     );
