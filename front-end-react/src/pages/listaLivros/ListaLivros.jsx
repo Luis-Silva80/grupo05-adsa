836bc34699c5379855dd9c8725c32eeaa9233bbc
lucas alves <lucas.pereira@codeby.com.br>
2021-10-23 23:07:47 -0300

    adding search by title, default image and preview at bookList

--- front-end-react/src/pages/listaLivros/ListaLivros.jsx
+++ front-end-react/src/pages/listaLivros/ListaLivros.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 // import components-
 import Footer from '../../components/footer/Footer';
 import SideBar from '../../components/sideBar/SideBar';
-
+import indisponivel from "../../assets/fotoIndisponivel.png"
 // function productShelf() {
 //     return(
 //         <>
@@ -36,7 +36,8 @@ function ListaLivros() {
             headers: { 'Content-Type': 'application/json', Accept: 'application/vnd.vtex.ds.v10+json' }
           };
           resp.innerHTML = ""
-          fetch(`https://codeby-cors.integrationby.com.br/https://www.googleapis.com/books/v1/volumes?q=${bookName}&maxResults=5`, options)
+          // alterar os valores do indice para implementar o scroll infinito    &startIndex=1
+          fetch(`https://codeby-cors.integrationby.com.br/https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&maxResults=40&startIndex=1`, options)
           .then(response => response.text())
           .then(result => {
             let data = JSON.parse(result)
@@ -47,14 +48,15 @@ function ListaLivros() {
             data.items.forEach(element => {
       
               resp.innerHTML +=
-                `
-                <div class="book">
-                  <img class="book_img" src="${element.volumeInfo.imageLinks.thumbnail}">
-                  <h3 class="book_title">${element.volumeInfo.title}</h3>
-                  <a href="/cadastroUsuario" class="book_btn">Ver mais</a>
-                  </div>
-                  `
-                });
+              `
+                <div class="resp_book">
+                <img class="resp_book_img" src="${element.volumeInfo.imageLinks ? element.volumeInfo.imageLinks.thumbnail : `${indisponivel}`}">
+                ${ element.volumeInfo.previewLink ? `<a class="resp_book_tag" target="_blank" href='${element.volumeInfo.previewLink}'>Preview</a>` : ""}
+                <h3 class="resp_book_title">${element.volumeInfo.title}</h3>
+                <a href="/cadastroUsuario" class="resp_book_btn">Ver mais</a>
+                </div>
+              `
+            });
           })
           .catch(err => console.error(err));
         }
@@ -67,8 +69,8 @@ function ListaLivros() {
     return (
         <>
             <SideBar />
-            <main class="main container" id="rootListaLivro">
-                <h1 class="main_title">Livros cadastrados</h1>
+            <main className="main container" id="rootListaLivro">
+                <h1 className="main_title">Livros cadastrados</h1>
 
                 <div className="main_nav">
                     <input className="main_nav_input" placeholder="Digite o nome do livro" type="text" name="name" id="name" />
@@ -82,7 +84,6 @@ function ListaLivros() {
                 </div>
                 <h3 id="searchCallback" className="main_callback"></h3>
                 <div id="resp" className="resp"></div>
-
             </main>
             <Footer />
         </>
