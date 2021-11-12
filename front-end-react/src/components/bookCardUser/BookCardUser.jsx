e5dc2c29cd49df434a7d37612ea4484805be0c1b
lucas alves <lucas.pereira@codeby.com.br>
2021-11-12 15:28:38 -0300

    adding a new component for books and linking it

--- front-end-react/src/components/bookCardUser/BookCardUser.jsx
+++ front-end-react/src/components/bookCardUser/BookCardUser.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import './style.scss';
 
+import Popup from '../../components/popup/Popup';
 import { Link } from 'react-router-dom';
 
 
 
-function BookCard( props ) {
+function BookCardUser( props ) {
+    const [visible, setVisible] = useState(false);
+
     return(
         
         <>
+            
+            <Popup visible={visible} onClose={() => setVisible(false)} titulo="teste de props"/>
+
             <div class="book">
                 <Link to="./livro" class="book_link">
                     <img src={props.image} class="book_link_img" alt="book preview"/>
                 </Link>
                 <div class="book_info">
                     <h4 class="book_info_title">{props.titulo}</h4>
-                    <p class="book_info_desc">{props.descricao}</p>
-                    <Link to="/livro" class="book_info_btn">Ver Mais</Link>
+                    <p class="book_info_date devolution">Devolver até: <span id="devolutionDate">{props.date}</span></p>
+                    <button class="book_info_btn" onClick={() => setVisible(true)}>Ver Reserva</button>
                 </div>
             </div>
 
@@ -24,4 +30,4 @@ function BookCard( props ) {
     );
 }
 
-export default BookCard;
+export default BookCardUser;
