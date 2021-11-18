217d119c68338b4d47900ab725498d427da19152
MatheusA <matheus.garcia@codeby.com.br>
2021-11-18 17:45:40 -0300

    fix: arrumei a sideBar

--- front-end-react/src/components/sideBar/SideBar.jsx
+++ front-end-react/src/components/sideBar/SideBar.jsx
@@ -18,86 +18,59 @@ import logoImg from "../../assets/logoWhiteWhite.png";
 function SideBarComponent() {
 
     const history = useHistory();
-    const [ isSelect, setIsSelect ] = useState(true);
-    const [ HeaderOpen, setHeaderOpen ] = useState();
-    const [ headerIsSelect, setHeaderIsSelect ] = useState(false);
-
+    const [ headerIsSelect, setHeaderIsSelect ] = useState(window.screen.width < 767 ? false : true);
+    
     useEffect(() => {
-        if (window.screen.width < 767) {
-            setHeaderOpen(false);
-        } else {
-            setHeaderOpen(true);
+        switch (window.location.pathname) {
+            case "/perfilUsuario":
+                document.getElementById("perfilUsuario").classList.add('currentPage');
+                break;
+            case "/listaLivros":
+                document.getElementById("listaLivros").classList.add('currentPage');
+                break;
+            case "/contato":
+                document.getElementById("contato").classList.add('currentPage');
+                break;
+            case "/faq":
+                document.getElementById("faq").classList.add('currentPage');
+                break;
+            case "/listaUsuarios":
+                document.getElementById("listaUsuarios").classList.add('currentPage');
+                break;
+            case "/cadastroLivro":
+                document.getElementById("cadastroLivro").classList.add('currentPage');
+                break;
+            default:
+                break;
         }
-    },[])
-
-
-    const handleClick = useCallback(() => {
-        setIsSelect(!isSelect);
-    }, [isSelect])
+    })
     
     const handleClickHeader = useCallback(() => {
         setHeaderIsSelect(!headerIsSelect);
-        setHeaderOpen(true);
     }, [headerIsSelect])
-
-    const handleClickOpen = useCallback(() => {
-        setHeaderOpen(!HeaderOpen);
-    }, [HeaderOpen])
 
     useEffect(() => {
         if (headerIsSelect) {
             document.getElementById("rootSideBar").style.left = "0";
-        } else if(!headerIsSelect) {
+            if (window.screen.width < 767) {
+                IconFunc();
+            }
+        } else {
             document.getElementById("rootSideBar").style.left = "-300px";
         }
     }, [headerIsSelect])
 
-    useEffect(() => {
-        if (isSelect) {
-            switch (window.location.pathname) {
-                case "/perfilUsuario":
-                    document.getElementById("perfilUsuario").classList.add('currentPage');
-                    break;
-                case "/listaLivros":
-                    document.getElementById("listaLivros").classList.add('currentPage');
-                    break;
-                case "/contato":
-                    document.getElementById("contato").classList.add('currentPage');
-                    break;
-                case "/faq":
-                    document.getElementById("faq").classList.add('currentPage');
-                    break;
-                case "/listaUsuarios":
-                    document.getElementById("listaUsuarios").classList.add('currentPage');
-                    break;
-                case "/cadastroLivro":
-                    document.getElementById("cadastroLivro").classList.add('currentPage');
-                    break;
-                default:
-                    break;
-            }
-        }
-    }, [isSelect])
+    function IconFunc() {
+        const side = document.getElementById("rootSideBar");
+        side.classList.add('sideOpen');
+    }
 
-    useEffect(() => {
-        if (HeaderOpen) {
-            const side = document.getElementById("rootSideBar");
-            side.classList.add('sideOpen');
-        } else {
+    function IconFuncOut() {
+        if (window.screen.width > 767) {
             const side = document.getElementById("rootSideBar");
             side.classList.remove('sideOpen');
         }
-    },[HeaderOpen])
-
-    // function IconFunc() {
-    //     const side = document.getElementById("rootSideBar");
-    //     side.classList.add('sideOpen');
-    // }
-
-    // function IconFuncOut() {
-    //     const side = document.getElementById("rootSideBar");
-    //     side.classList.remove('sideOpen');
-    // }
+    }
 
     function LogOut() {
         api
@@ -120,29 +93,29 @@ function SideBarComponent() {
                 <img onClick={handleClickHeader} className="headerMobile_hamburger" src={hamburgerMenu} />
                 <img className="headerMobile_logo" src={logoImg} />
             </header>
-            <aside onMouseOver={handleClickOpen} onMouseOut={handleClickOpen} className="sideBar" id="rootSideBar">
+            <aside onMouseOver={IconFunc} onMouseOut={IconFuncOut} className="sideBar" id="rootSideBar">
                 <div className="sideBar_containerCategory">
-                    <Link onClick={handleClick} to="/perfilUsuario" className="sideBar_containerCategory_link" id="perfilUsuario">
+                    <Link to="/perfilUsuario" className="sideBar_containerCategory_link" id="perfilUsuario">
                         <img src={userIcon} className="sideBar_containerCategory_link_icon" />
                         <p className="sideBar_containerCategory_link_desc">Perfil Usuário</p>
                     </Link>
-                    <Link onClick={handleClick} to="/listaLivros" className="sideBar_containerCategory_link" id="listaLivros">
+                    <Link to="/listaLivros" className="sideBar_containerCategory_link" id="listaLivros">
                         <img src={bookIcon} className="sideBar_containerCategory_link_icon" />
                         <p className="sideBar_containerCategory_link_desc">Pesquisar Livros</p>
                     </Link>
-                    <Link onClick={handleClick} to="/contato" className="sideBar_containerCategory_link" id="contato">
+                    <Link to="/contato" className="sideBar_containerCategory_link" id="contato">
                         <img src={email} className="sideBar_containerCategory_link_icon" />
                         <p className="sideBar_containerCategory_link_desc">Contate-nos</p>
                     </Link>
-                    <Link onClick={handleClick} to="/faq" className="sideBar_containerCategory_link" id="faq">
+                    <Link to="/faq" className="sideBar_containerCategory_link" id="faq">
                         <img src={questionIcon} className="sideBar_containerCategory_link_icon" />
                         <p className="sideBar_containerCategory_link_desc">Perguntas frequentes</p>
                     </Link>
-                    <Link onClick={handleClick} to="/listaUsuarios" className="sideBar_containerCategory_link" id="listaUsuarios">
+                    <Link to="/listaUsuarios" className="sideBar_containerCategory_link" id="listaUsuarios">
                         <img src={listUser} className="sideBar_containerCategory_link_icon" />
                         <p className="sideBar_containerCategory_link_desc">Lista de Usuários</p>
                     </Link>
-                    <Link onClick={handleClick} to="/cadastroLivro" className="sideBar_containerCategory_link" id="cadastroLivro">
+                    <Link to="/cadastroLivro" className="sideBar_containerCategory_link" id="cadastroLivro">
                         <img src={addLivro} className="sideBar_containerCategory_link_icon" />
                         <p className="sideBar_containerCategory_link_desc">Adicionar Livro</p>
                     </Link>
